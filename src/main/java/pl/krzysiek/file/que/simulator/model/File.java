d2a3ca0aa16bd329046d6729053d8bd9208c08bb
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2022-04-06 10:21:56 +0200

    06-04-2022 finished simulation, implement math formula

--- src/main/java/pl/krzysiek/file/que/simulator/model/File.java
+++ src/main/java/pl/krzysiek/file/que/simulator/model/File.java
@@ -2,25 +2,28 @@ package pl.krzysiek.file.que.simulator.model;
 
 import lombok.Data;
 
-import java.util.Objects;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Data
-public class File {
+public class File{
 
     public static int id = 0;
 
     private String filename;
     private Integer sizeOfFile;
+    private LocalDateTime startWaitingTime;
+    private Double priority;
 
     public void initFile() {
         Random generator = new Random();
         filename = "file_" + id++;
         sizeOfFile = generator.nextInt(2, 100);
+        startWaitingTime = LocalDateTime.now().minusHours(generator.nextInt(100));
     }
 
     @Override
     public String toString() {
-        return filename + " size: " + sizeOfFile + "B ";
+        return filename + " size: " + sizeOfFile + "B " + priority;
     }
 }
--- src/main/java/pl/krzysiek/file/que/simulator/model/QueHandler.java
+++ src/main/java/pl/krzysiek/file/que/simulator/model/QueHandler.java
@@ -2,6 +2,8 @@ package pl.krzysiek.file.que.simulator.model;
 
 import lombok.Data;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,7 +13,6 @@ import java.util.stream.Collectors;
 public class QueHandler implements Runnable {
 
     private UserContainer userContainer;
-    //    List<Thread> hardThreads  = new LinkedList<>();
     private List<HardDrive> hardDrives = new LinkedList<>();
 
 
@@ -20,25 +21,33 @@ public class QueHandler implements Runnable {
         while (true) {
             var freeHardDrives = hardDrives
                     .stream().filter(HardDrive::getFree).collect(Collectors.toList());
-//            System.out.println("wolne dyski: " + freeHardDrives);
             if (!freeHardDrives.isEmpty()) {
                 HardDrive hardDrive = freeHardDrives.get(0);
-                if (!userContainer.getUsers().get(0).getFiles().isEmpty()) {
-                    LinkedList<File> files = new LinkedList<>();
-                    for ( var user : userContainer.getUsers() ){
-                        if ( !user.getFiles().isEmpty() ){
-                            files.add(user.getFiles().get(0));
-                        }
-                    }
-                    files.sort(Comparator.comparingInt(File::getSizeOfFile));
-                    hardDrive.setActualFile(files.get(0));
-                    System.out.println(files.get(0));
-                    hardDrive.setActualFile(files.get(0));
+                LinkedList<File> files = new LinkedList<>();
+                userContainer.getUsers().stream()
+                        .filter(user -> !user.getFiles().isEmpty())
+                        .forEach(user -> files.add(user.getFiles().get(0)));
 
-                    userContainer.getUsers().forEach(user -> user.getFiles().remove(files.get(0)));
-                    hardDrive.setFree(false);
-                    new Thread(hardDrive).start();
+
+                for (var f : files) {
+                    long l = Duration.between(f.getStartWaitingTime(), LocalDateTime.now()).toHours();
+                    var temp = Math.sqrt(l) / userContainer.getUsers().size() + Math.pow(f.getSizeOfFile(), -1)
+                            * userContainer.getUsers().size();
+                    f.setPriority(temp);
                 }
+                files.sort(Comparator.comparing(File::getPriority, Comparator.reverseOrder()));
+                hardDrive.setActualFile(files.get(0));
+
+
+                userContainer.getUsers().forEach(user -> user.getFiles().remove(files.get(0)));
+                userContainer.setUsers(userContainer.getUsers().stream().filter(user -> !user.getFiles().isEmpty())
+                        .collect(Collectors.toList()));
+                hardDrive.setFree(false);
+                new Thread(hardDrive).start();
+
+            }
+            if (userContainer.getUsers().isEmpty()) {
+                break;
             }
             try {
                 Thread.sleep(1000);
